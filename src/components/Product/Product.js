8585801a9ef8e5ed95bfd57a8b4070b077941ef6
sn77sohan <sohan.ahmedself@gmail.com>
2021-09-26 21:36:34 +0600

    seeond-edition

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props)=> {
     // console.log(props);
     const {name, img,seller,price, stock} = props.product;
+    const element = <FontAwesomeIcon icon={faShoppingCart} />;
     return (
         <div className="product">
             <div>
@@ -16,7 +19,7 @@ const Product = (props)=> {
                 <p><small>only {stock} left in stock - order soon</small></p>
                 <button 
                 onClick ={()=>props.handleAddToCart(props.product)}
-                className="btn-regular">Add to cart</button>
+                className="btn-regular">{element}Add to cart</button>
           </div>
         </div>
     )
